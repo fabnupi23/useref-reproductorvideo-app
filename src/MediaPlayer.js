6227689f10ba435f465e8d833b1eb40bd9d2a2b1
Fabian_Nuñez <31360808+fabnupi23@users.noreply.github.com>
2022-09-20 11:55:58 -0500

    Play-Pause de video con hooks useRed-useState

--- src/MediaPlayer.js
+++ src/MediaPlayer.js
@@ -1,18 +1,24 @@
-import React, { useRef } from 'react'
+import React, { useState, useRef } from 'react'
 
 function MediaPlayer() {
     const videoRef = useRef();
+    const [isPlaying, setIsPlaying] = useState(false);
+
     const handlePlay = () => {
         const video = videoRef.current;
-        video.play();
+
+        isPlaying ? video.pause() : video.play();
+        setIsPlaying(!isPlaying);
     }
+
+
   return (
     <div>
-        <video width="400" ref={videoRef}>
+        <video width="400" ref={videoRef} onClick={handlePlay}>
             <source src='videos/aprender.mp4' type='video/mp4'/>
         </video>
         <button onClick={handlePlay}>
-            Play 
+            { isPlaying ? 'Pause' : 'Play' } 
         </button>
     </div>
   )
