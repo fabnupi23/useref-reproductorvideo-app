b3cf7e6ead7064a29f9f24d52ea9eca95cc9afe1
Fabian_Nuñez <31360808+fabnupi23@users.noreply.github.com>
2022-09-20 10:33:41 -0500

    Estructura proyecto control MediaPlayer-useRef

---
+++ src/MediaPlayer.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function MediaPlayer() {
+  return (
+    <div>
+        <h1>
+            Soy MediaPlayer
+        </h1>
+    </div>
+  )
+}
+
+export { MediaPlayer };
